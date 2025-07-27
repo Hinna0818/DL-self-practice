9daa68c1019499189e664178b01d855267c3ca6a
Hinna0818 <hinna@i.smu.edu.cn>
2025-07-27 16:15:26 +0800

    add

---
+++ nlp/utils.py
@@ -0,0 +1,108 @@
+import numpy as np
+
+def preprocess(text):
+
+    """Preprocess the input text to create a corpus and mappings from words to IDs and vice versa."""
+
+    text = text.lower()
+    text = text.replace(".", " .")
+    words = text.split(" ")
+
+    word2id, id2word = {}, {}
+    for word in words:
+        if word not in word2id:
+            new_id = len(word2id)
+            word2id[word] = new_id
+            id2word[new_id] = word
+    
+    corpus = np.array([word2id[word] for word in words])
+
+    return corpus, word2id, id2word
+
+
+# # preprocess function Example usage
+# if __name__ == "__main__":
+#     text = "You say goodbye and I say hello."
+#     corpus, word2id, id2word = preprocess(text)
+    
+#     print("Corpus:", corpus)
+#     print("Word to ID mapping:", word2id)
+#     print("ID to Word mapping:", id2word)
+
+
+def create_co_matrix(corpus, vocab_size, window_size = 1):
+
+    """Create a co-occurrence matrix from the corpus."""
+
+    corpus_size = len(corpus)
+    co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
+
+    for idx, word_id in enumerate(corpus):
+        for i in range(1, window_size + 1):
+            left_idx = idx - i
+            right_idx = idx + i
+
+            if left_idx >= 0:
+                left_word_id = corpus[left_idx]
+                co_matrix[word_id, left_word_id] += 1
+            
+            if right_idx < corpus_size:
+                right_word_id = corpus[right_idx]
+                co_matrix[word_id, right_word_id] += 1
+    
+    return co_matrix
+
+
+def cos_similarity(x, y, eps = 1e-8):
+
+    """Calculate the cosine similarity between two vectors."""
+
+    nx = x / np.sqrt(np.sum(x ** 2) + eps)
+    ny = y / np.sqrt(np.sum(y ** 2) + eps)  
+    return np.dot(nx, ny)
+
+
+def most_similar(query, word2id, id2word, co_matrix, top=5):
+    """Find the most similar words to the query word based on the co-occurrence matrix."""
+
+    if query not in word2id:
+        print(f"{query} is not found in the vocabulary.")
+        return None
+    
+    query_id = word2id[query]
+    query_vec = co_matrix[query_id]
+
+    similarity = np.zeros(len(id2word))
+    for i in range(len(id2word)):
+        similarity[i] = cos_similarity(query_vec, co_matrix[i])
+    
+    count = 0
+    for i in (-1 * similarity).argsort():
+        if id2word[i] == query:
+            continue
+        print(f"{count + 1}: {id2word[i]}: {similarity[i]:.4f}")
+        count += 1
+        if count >= top:
+            return
+
+
+def ppmi(C, verbose = False, eps = 1e-8):
+    """
+    Calculate the Positive Pointwise Mutual Information (PPMI) matrix from the co-occurrence matrix C."""
+
+    M = np.zeros_like(C, dtype=np.float32)
+    N = np.sum(C)
+    S = np.sum(C, axis=0)
+    total = C.shape[0] * C.shape[1]
+    cnt = 0
+
+    for i in range(C.shape[0]):
+        for j in range(C.shape[1]):
+            pmi = np.log2((C[i, j] * N) / (S[j] * S[i]) + eps)
+            M[i, j] = max(0, pmi)
+
+            if verbose:
+                cnt += 1
+                if cnt % (total // 100) == 0:
+                    print(f"{cnt / total * 100:.1f}% done")
+    return M
